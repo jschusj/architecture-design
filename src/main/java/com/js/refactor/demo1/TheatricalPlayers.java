dc04e525f07fe662570faf24aec55810627872b7
YangTao <18268100534@163.com>
2025-03-12 23:28:54 +0800

    refactor: ResultFormatter方法对象

--- src/main/java/com/js/refactor/demo1/TheatricalPlayers.java
+++ src/main/java/com/js/refactor/demo1/TheatricalPlayers.java
@@ -1,8 +1,5 @@
 package com.js.refactor.demo1;
 
-import java.text.NumberFormat;
-import java.util.Locale;
-
 /**
  * @Author：YangTao
  * @Date：2025/3/12 22:55
@@ -11,14 +8,7 @@ public class TheatricalPlayers {
     public String print(Invoice invoice) {
         int totalAmount = new TotalAmountCalculator().getTotalAmount(invoice);
         int volumeCredits = new VolumeCreditsCalculator().getVolumeCredits(invoice);
-        return getResult(invoice, totalAmount, volumeCredits);
+        return new ResultFormatter().getResult(invoice, totalAmount, volumeCredits);
     }
 
-    private String getResult(Invoice invoice, int totalAmount, int volumeCredits) {
-        String result = String.format("Statement for %s\n", invoice.customer);
-        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
-        result += String.format("Amount owed is %s\n", format.format(totalAmount / 100));
-        result += String.format("You earned %s credits\n", volumeCredits);
-        return result;
-    }
 }
